a768ff2708fccc3ea833aed80c2906a114856bb7
Alex Katz <alexanderikatz@gmail.com>
2023-08-09 20:09:35 -0400

    placed place next to google maps iframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,12 +105,13 @@ export default function Home() {
           <section.h3>Time</section.h3>
           <section.p>{'Ceremony at 4pm'}</section.p>
 
+          <section.h3>Dress</section.h3>
+          <section.p>{'Cocktail Attire'}</section.p>
+
           <section.h3>Place</section.h3>
           <section.p>{'Pier Sixty'}</section.p>
           <section.p>{'60 Chelsea Piers, New York, NY 10011'}</section.p>
 
-          <section.h3>Dress</section.h3>
-          <section.p>{'Cocktail Attire'}</section.p>
           <iframe
             src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.698364337545!2d-74.0107728!3d40.7466623!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259c7dace3e8d%3A0xb21e63fb78aa793e!2sPier%20Sixty!5e0!3m2!1sen!2sus!4v1686501696537!5m2!1sen!2sus'
             width='vw100'
